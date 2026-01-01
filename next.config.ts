60ecda405239b7582e6c49d8da5b128d86f38325
パコソン\kazum <toypoooooodle@gmail.com>
2026-01-01 21:19:58 +0900

    テスト

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Cloudflare Pagesで動かすためにこれが必要な場合があります
-  // output: 'export', // 静的書き出しモードにしているか確認
   images: {
     unoptimized: true,
   },
